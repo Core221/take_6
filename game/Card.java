5ef441003f1ace748ed9586d462bb57c10c37c66
johnnyb <j.buerst@gmx.de>
2016-06-10 14:56:24 +0200

    "Card berechnet nun Bullenanzahl"

--- game/Card.java
+++ game/Card.java
@@ -5,9 +5,18 @@ public class Card{
     private int points;
     private int value;
 
-    public Card(int value, int points){
-        this.points = points;
+    public Card(int value){
+
         this.value = value;
+        String doubles = value+"";
+        if((value%5)==0 && value%10!=0)points=2;
+        else if((value%10)==0 && (value%5)!=0 )points=3;
+        else if(doubles.charAt(0)==doubles.charAt(1))points=5;
+        else if(value==55)points=7;
+        else{
+            points =1;
+        }
+    System.out.println(points);
     }
 
     public int getPoints(){
