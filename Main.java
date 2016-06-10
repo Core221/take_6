e0291005f8b8c432277d2eb30199fe9c9e51ed1e
johnnyb <j.buerst@gmx.de>
2016-06-10 15:05:01 +0200

    "Card berechnet nun Bullenanzahl(Korrektur)"

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 public class Main{
 
 	public static void main(String[] args){
+
 		TakeSix game = new TakeSix();
 		Application.launch(TakeSix.class, args);
 	}
--- game/Card.java
+++ game/Card.java
@@ -11,12 +11,14 @@ public class Card{
         String doubles = value+"";
         if((value%5)==0 && value%10!=0)points=2;
         else if((value%10)==0 && (value%5)!=0 )points=3;
-        else if(doubles.charAt(0)==doubles.charAt(1))points=5;
+        else if(doubles.length()==2) {
+            if (doubles.charAt(0) == doubles.charAt(1)) points = 5;
+        }
         else if(value==55)points=7;
         else{
             points =1;
         }
-    System.out.println(points);
+         System.out.println(points);
     }
 
     public int getPoints(){
